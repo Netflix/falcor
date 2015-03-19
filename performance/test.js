18c4b61b3368acc4e56f73d9dd1860b514e17a32
Michael Paulson <mpaulson@netflix.com>
2015-03-19 12:29:51 -0700

    One last update to perf

--- performance/test.js
+++ performance/test.js
@@ -6,6 +6,14 @@ var models = testConfig.models;
 
 testConfig.repeatInConfig('sentinel.Model simple', 1, testConfig.simple(models.sentinel, 'JSON'), config.tests);
 testConfig.repeatInConfig('alternate.Model simple', 1, testConfig.simple(models.alt, 'JSON'), config.tests);
+testConfig.repeatInConfig('sentinel.Model reference', 1, testConfig.reference(models.sentinel, 'JSON'), config.tests);
+testConfig.repeatInConfig('alternate.Model reference', 1, testConfig.reference(models.alt, 'JSON'), config.tests);
+testConfig.repeatInConfig('sentinel.Model complex', 1, testConfig.complex(models.sentinel, 'JSON'), config.tests);
+testConfig.repeatInConfig('alternate.Model complex', 1, testConfig.complex(models.alt, 'JSON'), config.tests);
+testConfig.repeatInConfig('sentinel.Model startup', 1, testConfig.startup(models.sentinel, 'JSON'), config.tests);
+testConfig.repeatInConfig('alternate.Model startup', 1, testConfig.startup(models.alt, 'JSON'), config.tests);
+testConfig.repeatInConfig('sentinel.Model scrollGallery', 1, testConfig.scrollGallery(models.sentinel, 'JSON'), config.tests);
+testConfig.repeatInConfig('alternate.Model scrollGallery', 1, testConfig.scrollGallery(models.alt, 'JSON'), config.tests);
 
 require('./test-header')(require('benchmark'), config, 5, function(totalResults) {
     var fs = require('fs');
