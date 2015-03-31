aa4f2073f5a3cddb812918f41ffbd3dab7efd4e6
Paul Taylor <ptaylor@netflix.com>
2015-03-30 18:03:04 -0700

    Adds tests for setting multiple paths and a partial JSON-Graph envelope.

--- test/set/index.js
+++ test/set/index.js
@@ -1,6 +1,6 @@
 // describe.only('Set', function() {
 describe('Set', function() {
-    require('./set.core.spec');
+    // require('./set.core.spec');
     // require('./get.sync.spec');
     require('./edge-cases.spec');
     require('./set.json-tests.spec');
