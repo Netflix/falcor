d475118b38729f870d60f14b645ead6090e6a9a7
Paul Taylor <ptaylor@netflix.com>
2015-04-02 14:21:18 -0700

    Adds bound path support to setJSONGsAs* methods.

--- test/set/support/set-envelopes.js
+++ test/set/support/set-envelopes.js
@@ -8,15 +8,18 @@ var partial_cache = require("./partial-cache");
 function set_envelopes(envelopes, suffix, options) {
     var model   = options && options.model || new Model(_.extend({ cache: partial_cache() }, options || {}));
     var seeds   = suffix == "JSON" ? get_seeds(envelopes.flatMap(get_paths)) : [{}];
-    if(suffix == "Values") {
+    if(suffix == "JSONG") {
+        model._path = [];
+    } else if(suffix == "Values") {
         var values = [];
         seeds = function(pv) { values.push(pv); }
     }
+    debugger;
     var func = model["_setJSONGsAs" + suffix];
     var results = func(model, envelopes, seeds);
     if(values) { results.values = values; }
     
-    // console.log(require("util").inspect(results.values[0], {depth: null}));
+    // console.log(require("util").inspect(results.values, {depth: null}));
     
     return [model, results];
 }
