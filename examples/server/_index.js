20daf863e540f3484761d3c491f7542f886723d7
Michael Paulson <mpaulson@netflix.com>
2015-04-14 14:39:00 -0700

    the server with the paste-able upgrade for falcor.

---
+++ examples/server/_index.js
@@ -0,0 +1,32 @@
+var Falcor = require('./../Falcor');
+var FalcorServer = require('falcor-server');
+var model = new Falcor.Model({cache: {
+    genreLists: [
+        {
+            name: 'Recently Watched',
+            titles: [
+                {
+                    id: 1007,
+                    name: 'Bloodline',
+                    rating: 4,
+                    boxshot: '../assets/bl.webp'
+                }
+                //, ... more titles
+            ]
+        },
+        {
+            name: 'New Releases',
+            titles: [
+                {
+                    id: 956,
+                    name: 'House of Cards',
+                    rating: 4,
+                    boxshot: '../assets/hoc.webp'
+                }
+                //, ... more titles
+            ]
+        }
+        //, ... more genres
+    ]
+}});
+app.use('/member.json', FalcorServer.ExpressMiddleware(model));
