0d4130ce8e7aa5387a983685aadfbc15fd9bc67f
Michael Paulson <mpaulson@netflix.com>
2015-04-14 19:33:33 -0700

    updated with path syntax stuff and updated the server.

--- test/integration/end-to-end/express-falcor.js
+++ test/integration/end-to-end/express-falcor.js
@@ -9,8 +9,8 @@ var fullCacheModel = new falcor.Model({cache: Cache()}).materialize();
 var edgeCaseModel = new falcor.Model({cache: edgeCaseCache}).materialize();
 
 // Simple middleware to handle get/post
-app.use('/falcor', FalcorServer.ExpressMiddleware(fullCacheModel));
-app.use('/falcor.edge', FalcorServer.ExpressMiddleware(edgeCaseModel));
+app.use('/falcor', FalcorServer.expressMiddleware(function() { return fullCacheModel; }));
+app.use('/falcor.edge', FalcorServer.expressMiddleware(function() { return edgeCaseModel; }));
 
 module.exports = function(port) {
     // Note: Will never complete unless explicitly closed.
