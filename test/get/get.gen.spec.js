7d85c30a609b5861192df46695bf81fa6aeae1d4
Michael Paulson <mpaulson@netflix.com>
2015-05-14 16:40:13 -0700

    removed push

---
+++ test/get/get.gen.spec.js
@@ -0,0 +1,18 @@
+var jsong = require("../../index");
+var Model = jsong.Model;
+var expect = require('chai').expect;
+
+describe('getGenerationSync', function() {
+    it('should get a generation', function() {
+        var model = new Model({cache: {hello: 'world'}});
+        model._root.unsafeMode = true;
+        var gen = model.getGenerationSync('hello');
+        expect(gen > 0).to.be.ok;
+    });
+    it('should get an undefined if no path exists.', function() {
+        var model = new Model({cache: {hello: 'world'}});
+        model._root.unsafeMode = true;
+        var gen = model.getGenerationSync('world');
+        expect(gen === undefined).to.be.ok;
+    });
+});
