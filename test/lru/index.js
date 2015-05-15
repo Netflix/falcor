d95277429b12758b51d98371cb575337e0e88ef2
Michael Paulson <mpaulson@netflix.com>
2015-05-15 05:30:40 -0700

    the tests are passing as of now.

--- test/lru/index.js
+++ test/lru/index.js
@@ -1,6 +1,7 @@
 describe("LRU", function() {
     describe('Promote', function() {
         require("./lru.promote.get.spec");
+        require("./lru.promote.set.spec");
     });
     describe('Splice', function() {
         require("./lru.splice.expired.spec");
