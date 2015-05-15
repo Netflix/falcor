1fd9b6ca239c37ea103bd91dea2bf5895ba0bc39
Michael Paulson <mpaulson@netflix.com>
2015-05-15 05:58:54 -0700

    our eslint was out of date from the source of truth and the LRU is not working correctly on all of the sets.

--- test/lru/index.js
+++ test/lru/index.js
@@ -1,10 +1,11 @@
-describe("LRU", function() {
+describe.only("LRU", function() {
     describe('Promote', function() {
         require("./lru.promote.get.spec");
         require("./lru.promote.set.spec");
     });
     describe('Splice', function() {
         require("./lru.splice.expired.spec");
+        require("./lru.splice.overwrite.spec");
     });
 });
 
