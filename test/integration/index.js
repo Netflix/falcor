45f4a3893122b4cf0d4585d4e254728467067d84
Michael Paulson <mpaulson@netflix.com>
2015-05-27 14:18:40 -0700

    Removed the integration tests as they have fallen out of window.

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,4 +1,5 @@
 describe("Integration", function() {
+    // TODO: Unsure how much of these tests we need to write to begin with.
     //require('./router');
-    require('./end-to-end');
+    //require('./end-to-end');
 });
