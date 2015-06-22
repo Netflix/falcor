f6985c13688571203e0329433750328759fa7380
Satyen Desai <satyend@netflix.com>
2015-06-22 13:19:25 -0700

    Refactor 1st pass

--- performance/node.js
+++ performance/node.js
@@ -1,51 +1,34 @@
-var Rx = require('rx');
-var benchmark = require('benchmark');
 var testRunner = require('./testRunner');
-global.Rx = Rx;
+var testSuiteGenerator = require('./testSuiteGenerator');
 var testConfig = require('./testConfig')();
-var config = testConfig.config;
+
 var models = testConfig.models;
+var formats = testConfig.formats;
+var tests = testConfig.get;
+var suite = testConfig.suite;
 
-// var mdpSimple = testConfig.get.simple(models.mdp, 'Value');
-//var modelSimple = testConfig.get.simple(models.model, 'Value');
-//var mdpReference = testConfig.get.reference(models.mdp, 'Value');
-//var modelReference = testConfig.get.reference(models.model, 'Value');
-//var mdpComplex = testConfig.get.complex(models.mdp, 'Value');
-//var modelComplex = testConfig.get.complex(models.model, 'Value');
-//var mdpScrollGallery = testConfig.get.scrollGallery(models.mdp, 'Value');
-//var modelScrollGallery = testConfig.get.scrollGallery(models.model, 'Value');
-// mdpSimple();
+suite.tests = testSuiteGenerator({
 
-// testConfig.repeatInConfig('mdp-sync-simple', 1, mdpSimple, config.tests);
-//testConfig.repeatInConfig('model-sync-simple', 15, modelSimple, config.tests);
-//testConfig.repeatInConfig('mdp-sync-reference', 15, mdpReference, config.tests);
-//testConfig.repeatInConfig('model-sync-reference', 15, modelReference, config.tests);
-//testConfig.repeatInConfig('mdp-sync-complex', 15, mdpComplex, config.tests);
-//testConfig.repeatInConfig('model-sync-complex', 15, modelComplex, config.tests);
-//testConfig.repeatInConfig('mdp-sync-gallery', 15, mdpScrollGallery, config.tests);
-//testConfig.repeatInConfig('model-sync-gallery', 15, modelScrollGallery, config.tests);
+    iterations: 1,
 
-var cache = require('./../test/data/Cache')();
-var model = require("../lib/support/test-model")(cache);
+    models: {
+        'model' : models.model,
+        'mock' : models.mock
+    },
 
-model._getPathSetsAsValues  = require("../lib/json-values/get-path-sets");
-model._getPathSetsAsJSON    = require("../lib/json-dense/get-path-sets");
-model._getPathSetsAsPathMap = require("../lib/json-sparse/get-path-sets");
-model._getPathSetsAsJSONG   = require("../lib/json-graph/get-path-sets");
+    formats: [
+        'JSON',
+        'Value',
+        'PathMap',
+        'JSONG'
+    ],
 
-testConfig.repeatInConfig('gallery values mbp', 1, testConfig.get.scrollGallery(models.model, 'Value'), config.tests);
-testConfig.repeatInConfig('gallery values pet', 1, testConfig.get.scrollGallery(model, 'Value'), config.tests);
+    tests: {
+        'gallery values': tests.scrollGallery
+    }
 
-testConfig.repeatInConfig('gallery json mbp', 1, testConfig.get.scrollGallery(models.model, 'JSON'), config.tests);
-testConfig.repeatInConfig('gallery json pet', 1, testConfig.get.scrollGallery(model, 'JSON'), config.tests);
+});
 
-testConfig.repeatInConfig('gallery pathmap mbp', 1, testConfig.get.scrollGallery(models.model, 'PathMap'), config.tests);
-testConfig.repeatInConfig('gallery pathmap pet', 1, testConfig.get.scrollGallery(model, 'PathMap'), config.tests);
-
-testConfig.repeatInConfig('gallery graph mbp', 1, testConfig.get.scrollGallery(models.model, 'JSONG'), config.tests);
-testConfig.repeatInConfig('gallery graph pet', 1, testConfig.get.scrollGallery(model, 'JSONG'), config.tests);
-
-testRunner(benchmark, config, 10, function(totalResults) {
-    var fs = require('fs');
-    fs.writeFileSync('out.csv', totalResults.join('\n'))
+testRunner(suite, 10, function(totalResults) {
+    require('fs').writeFileSync('out.csv', totalResults.join('\n'));
 });
