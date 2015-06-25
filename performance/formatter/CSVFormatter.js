0b7cf4524ffa1afcae5365a744651f6fbe5c28b1
Satyen Desai <satyend@netflix.com>
2015-06-25 16:55:39 -0700

    Working Karma Implementation

---
+++ performance/formatter/CSVFormatter.js
@@ -0,0 +1,46 @@
+var benchmarkToRow = function(benchmark, env) {
+
+    if (!env) {
+        env = "ENV";
+    }
+
+    return [
+        env,
+        benchmark.suite,
+        benchmark.name,
+        benchmark.hz,
+        benchmark.stats.deviation
+    ].join(', ');
+};
+
+var resultsToTable = function(results) {
+
+    var table =[];
+    var suites;
+    var suite;
+    var env;
+
+    for (env in results) {
+        suites = results[env];
+        for (suite in suites) {
+            suites[suite].forEach(function(benchmark) {
+                table.push(benchmarkToRow(benchmark, env));
+            });
+        }
+    }
+
+    return table.join('\n');
+};
+
+module.exports = {
+
+    toRow: benchmarkToRow,
+
+    toTable: resultsToTable,
+
+    pipe: function(resultsFormatter, writer) {
+        return function(results) {
+            writer(resultsFormatter(results));
+        };
+    }
+};
--- performance/reporters/nodeTestReporter.js
+++ performance/reporters/nodeTestReporter.js
@@ -1,5 +1,16 @@
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
-module.exports = function(totalResults) {
-    fs.writeFileSync('out.csv', totalResults);
+var filepath = 'out/node-benchmark.csv';
+
+module.exports = function(results) {
+    mkdirp(path.dirname(filepath), function (err) {
+        if (err) {
+            console.error('\nError writing file: ' + filepath);
+        } else {
+            fs.writeFileSync(filepath, results);
+            console.info('\nCreated output file: ' + filepath);
+        }
+    });
 };
