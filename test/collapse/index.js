04ec5cb28050df77f624b7306e67585aebffbdc0
Paul Taylor <ptaylor@netflix.com>
2015-06-29 19:01:55 -0700

    Collapse without JSON.stringify.

--- test/collapse/index.js
+++ test/collapse/index.js
@@ -16,8 +16,20 @@ describe("collapse", function() {
         pathmap["lolomo"]["13"] = Object.create(null);
         pathmap["lolomo"]["13"][__count] = 1;
 
+        pathmap["lolomo"]["14"] = Object.create(null);
+        pathmap["lolomo"]["14"][__count] = 1;
+
         pathmap["lolomo"]["13"]["summary"] = Object.create(null);
         pathmap["lolomo"]["13"]["summary"][__count] = 1;
+
+        pathmap["lolomo"]["14"]["summary"] = Object.create(null);
+        pathmap["lolomo"]["14"]["summary"][__count] = 1;
+
+        // pathmap["lolomo"]["15"] = Object.create(null);
+        // pathmap["lolomo"]["15"]["summary"] = Object.create(null);
+        // pathmap["lolomo"]["15"]["summary"][__count] = 1;
+
+        // debugger;
 
         var paths = collapse(pathmap).sort(function(a, b) {
             return a.length - b.length;
@@ -29,8 +41,20 @@ describe("collapse", function() {
         var fourth = paths[3];
 
         expect(first[0] === "lolomo").to.equal(true);
-        expect(second[0] === "lolomo" && second[1] === 13).to.equal(true);
+
+        expect((
+            second[0] === "lolomo")   && (
+            second[1]["from"] === 13) && (
+            second[1]["to"] === 14)
+        ).to.equal(true);
+
         expect(third[0] === "lolomo" && third[1] === "summary").to.equal(true);
-        expect(fourth[0] === "lolomo" && fourth[1] === 13 && fourth[2] === "summary").to.equal(true);
+
+        expect((fourth[0] === "lolomo") && (
+            fourth[1]["from"] === 13)   && (
+            fourth[1]["to"] === 14)     && (
+            // fourth[1]["to"] === 15)     && (
+            fourth[2] === "summary")
+        ).to.equal(true);
     });
 });
