a07bb1a2a2a8cc7b2b651737b6bf669acab40531
Michael Paulson <mpaulson@netflix.com>
2015-07-01 15:04:03 -0700

    the potential comparison.

--- performance/tests/get/index.js
+++ performance/tests/get/index.js
@@ -42,30 +42,30 @@ module.exports = {
             case 'JSON':
                 return function(done) {
                     model.get.apply(model, startupRequest, function(a,b,c,d,e,f,g,h,i,h,j,k,l,m,n,o,p,q,r,s,t) {
-                        done.resolve();
+                        done && done.resolve();
                     });
                 };
             case 'JSONG':
                 return function(done) {
-                    model.get(model, startupRequest).
+                    model.get.apply(model, startupRequest).
                         toJSONG().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'PathMap':
                 return function(done) {
-                    model.get(model, startupRequest).
+                    model.get.apply(model, startupRequest).
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'Value':
                 return function(done) {
-                    model.get(model, startupRequest).
+                    model.get.apply(model, startupRequest).
                         toPathValues().
                         subscribe(noOp, noOp, function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
         }
@@ -84,31 +84,31 @@ module.exports = {
                 return function(done) {
                     model.
                         get.apply(model, scrollingRequest.concat(function(a,b,c,d) {
-                            done.resolve();
+                            done && done.resolve();
                         })).
                         subscribe();
                 };
             case 'JSONG':
                 return function(done) {
-                    model.get(model, scrollingRequest).
+                    model.get.apply(model, scrollingRequest).
                         toJSONG().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'PathMap':
                 return function(done) {
-                    model.get(model, scrollingRequest).
+                    model.get.apply(model, scrollingRequest).
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'Value':
                 return function(done) {
-                    model.get(model, scrollingRequest).
+                    model.get.apply(model, scrollingRequest).
                         toPathValues().
                         subscribe(noOp, noOp, function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
         }
@@ -122,30 +122,30 @@ module.exports = {
             case 'JSON':
                 return function(done) {
                     model.get.apply(model, simpleRequest, function(a) {
-                        done.resolve();
+                        done && done.resolve();
                     });
                 };
             case 'JSONG':
                 return function(done) {
-                    model.get(model, simpleRequest).
+                    model.get.apply(model, simpleRequest).
                         toJSONG().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'PathMap':
                 return function(done) {
-                    model.get(model, simpleRequest).
+                    model.get.apply(model, simpleRequest).
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'Value':
                 return function(done) {
-                    model.get(model, simpleRequest).
+                    model.get.apply(model, simpleRequest).
                         toPathValues().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
         }
@@ -159,30 +159,30 @@ module.exports = {
             case 'JSON':
                 return function(done) {
                     model.get.apply(model, referenceRequest, function(a) {
-                        done.resolve();
+                        done && done.resolve();
                     });
                 };
             case 'JSONG':
                 return function(done) {
-                    model.get(model, referenceRequest).
+                    model.get.apply(model, referenceRequest).
                         toJSONG().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'PathMap':
                 return function(done) {
-                    model.get(model, referenceRequest).
+                    model.get.apply(model, referenceRequest).
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'Value':
                 return function(done) {
-                    model.get(model, referenceRequest).
+                    model.get.apply(model, referenceRequest).
                         toPathValues().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
         }
@@ -196,30 +196,30 @@ module.exports = {
             case 'JSON':
                 return function(done) {
                     model.get.apply(model, complexRequest, function(a) {
-                        done.resolve();
+                        done && done.resolve();
                     });
                 };
             case 'JSONG':
                 return function(done) {
-                    model.get(model, complexRequest).
+                    model.get.apply(model, complexRequest).
                         toJSONG().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'PathMap':
                 return function(done) {
-                    model.get(model, complexRequest).
+                    model.get.apply(model, complexRequest).
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
             case 'Value':
                 return function(done) {
-                    model.get(model, complexRequest).
+                    model.get.apply(model, complexRequest).
                         toPathValues().
                         subscribe(function(next) {
-                            done.resolve();
+                            done && done.resolve();
                         });
                 };
         }
