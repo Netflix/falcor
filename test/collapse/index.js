69d702231f8e23a425db01da157612ebad4e034c
Paul Taylor <ptaylor@netflix.com>
2015-06-30 18:48:23 -0700

    Fixes long-standing collapse bug when keys are both leaves and branches.

--- test/collapse/index.js
+++ test/collapse/index.js
@@ -1,37 +1,41 @@
 var expect = require("chai").expect;
 var collapse = require("falcor/support/collapse");
-var __count = require("falcor/internal/count");
 
 describe("collapse", function() {
     it("collapses a pathmap that has overlapping branch and leaf nodes", function() {
 
-        var pathmap = Object.create(null);
+        var pathmaps = [null, {
+            lolomo: 1
+        }, {
+            lolomo: {
+                13: 1,
+                14: 1,
+                summary: 1
+            }
+        }, {
+            lolomo: {
+                13: {
+                    summary: 1
+                },
+                14: {
+                    summary: 1
+                },
+                15: {
+                    rating: 1,
+                    summary: 1
+                },
+                16: {
+                    rating: 1,
+                    summary: 1
+                },
+                17: {
+                    rating: 1,
+                    summary: 1
+                }
+            }
+        }];
 
-        pathmap["lolomo"] = Object.create(null);
-        pathmap["lolomo"][__count] = 1;
-
-        pathmap["lolomo"]["summary"] = Object.create(null);
-        pathmap["lolomo"]["summary"][__count] = 1;
-
-        pathmap["lolomo"]["13"] = Object.create(null);
-        pathmap["lolomo"]["13"][__count] = 1;
-
-        pathmap["lolomo"]["14"] = Object.create(null);
-        pathmap["lolomo"]["14"][__count] = 1;
-
-        pathmap["lolomo"]["13"]["summary"] = Object.create(null);
-        pathmap["lolomo"]["13"]["summary"][__count] = 1;
-
-        pathmap["lolomo"]["14"]["summary"] = Object.create(null);
-        pathmap["lolomo"]["14"]["summary"][__count] = 1;
-
-        // pathmap["lolomo"]["15"] = Object.create(null);
-        // pathmap["lolomo"]["15"]["summary"] = Object.create(null);
-        // pathmap["lolomo"]["15"]["summary"][__count] = 1;
-
-        // debugger;
-
-        var paths = collapse(pathmap).sort(function(a, b) {
+        var paths = collapse(pathmaps).sort(function(a, b) {
             return a.length - b.length;
         });
 
@@ -44,16 +48,21 @@ describe("collapse", function() {
 
         expect((
             second[0] === "lolomo")   && (
-            second[1]["from"] === 13) && (
-            second[1]["to"] === 14)
+            second[1][0] === 13) && (
+            second[1][1] === 14) && (
+            second[1][2] === "summary")
         ).to.equal(true);
 
-        expect(third[0] === "lolomo" && third[1] === "summary").to.equal(true);
+        expect((third[0] === "lolomo") && (
+            third[1]["from"] === 15)   && (
+            third[1]["to"] === 17)     && (
+            third[2][0] === "rating")  && (
+            third[2][1] === "summary")
+        ).to.equal(true);
 
         expect((fourth[0] === "lolomo") && (
             fourth[1]["from"] === 13)   && (
             fourth[1]["to"] === 14)     && (
-            // fourth[1]["to"] === 15)     && (
             fourth[2] === "summary")
         ).to.equal(true);
     });
