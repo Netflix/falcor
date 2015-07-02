86f5887f8e7ffd8a835b65019c61187c07ed786a
Satyen Desai <satyend@netflix.com>
2015-07-01 18:09:29 -0700

    Refactoring the test harness to allow for onBenchmarkComplete variation across envs

--- performance/node.js
+++ performance/node.js
@@ -4,6 +4,16 @@ var testReporter = require('./reporters/nodeTestReporter');
 var testSuiteGenerator = require('./testSuiteGenerator');
 var CSVFormatter = require('./formatter/CSVFormatter');
 
+var compose = function(f, g) {
+    return function(v) {
+        return f(g(v));
+    };
+};
+
+var curry = function(fn, arg) {
+    return fn.bind(null, arg);
+};
+
 var models = testConfig.models;
 var formats = testConfig.formats;
 var tests = testConfig.get;
@@ -31,4 +41,8 @@ suite.tests = testSuiteGenerator({
 
 });
 
-testRunner(suite, 2, 'node ' + process.version, CSVFormatter.pipe(CSVFormatter.toTable, testReporter));
+var env = 'node ' + process.version;
+var resultsReporter = compose(testReporter.resultsReporter, CSVFormatter.toTable);
+var benchmarkReporter = compose(testReporter.benchmarkReporter, curry(CSVFormatter.toRow, env));
+
+testRunner(suite, 2, env, benchmarkReporter, resultsReporter);
