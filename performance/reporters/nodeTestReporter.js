860d8475b2daba7c8375ad166e3ce73ba862dd9f
Michael Paulson <mpaulson@netflix.com>
2015-07-02 10:53:41 -0700

    finishing up the node reporter to add name.

--- performance/reporters/nodeTestReporter.js
+++ performance/reporters/nodeTestReporter.js
@@ -2,10 +2,13 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
 var transformCSVResults = require('./transformCSVResults');
-var filepath = path.resolve(__dirname, '../out/node-benchmark.csv');
+var dirPath = path.resolve(__dirname, '../out');
+var DEFAULT_NAME = 'node-benchmark.csv';
+var COMMAND_OPTION = '-n';
 
 module.exports = function(results) {
     debugger
+    var filePath = path.resolve(dirPath, getFileName());
     results = transformCSVResults(results);
     mkdirp(path.dirname(filepath), function (err) {
         if (err) {
@@ -15,4 +18,22 @@ module.exports = function(results) {
             console.info('\nCreated output file: ' + filepath);
         }
     });
+};
+
+function getFileName() {
+    if (typeof process !== 'undefined') {
+        var name = DEFAULT_NAME;
+        var foundName = false;
+        process.argv.forEach(function(x) {
+            if (x === COMMAND_OPTION) {
+                foundName = true;
+            }
+
+            else if (foundName) {
+                name = x;
+            }
+        });
+        return foundName;
+    }
+    return DEFAULT_NAME;
 };
