a3492db04b5cf188ab7074d707c54ae37cd785cb
Michael Paulson <mpaulson@netflix.com>
2015-07-02 08:37:22 -0700

    starting the transfrom for CSV.

--- performance/reporters/nodeTestReporter.js
+++ performance/reporters/nodeTestReporter.js
@@ -1,23 +1,18 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
-
+var transformCSVResults = require('./transformCSVResults');
 var filepath = path.resolve(__dirname, '../out/node-benchmark.csv');
 
-module.exports = {
-
-    resultsReporter: function(results) {
-        mkdirp(path.dirname(filepath), function (err) {
-            if (err) {
-                console.error('\nError writing file: ' + filepath);
-            } else {
-                fs.writeFileSync(filepath, results);
-                console.info('\nCreated output file: ' + filepath);
-            }
-        });
-    },
-
-    benchmarkReporter: function(benchmark) {
-        console.info(benchmark);
-    }
+module.exports = function(results) {
+    debugger
+    results = transformCSVResults(results);
+    mkdirp(path.dirname(filepath), function (err) {
+        if (err) {
+            console.error('\nError writing file: ' + filepath);
+        } else {
+            fs.writeFileSync(filepath, results);
+            console.info('\nCreated output file: ' + filepath);
+        }
+    });
 };
