18cb27bab36e98bb6fbe5d7db996c9d6606a0daf
Michael Paulson <mpaulson@netflix.com>
2015-07-02 08:37:22 -0700

    starting the transfrom for CSV.

--- performance/reporters/nodeTestReporter.js
+++ performance/reporters/nodeTestReporter.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
-
+var transformCSVResults = require('./transformCSVResults');
 var filepath = path.resolve(__dirname, '../out/node-benchmark.csv');
 
 module.exports = function(results) {
+    debugger
+    results = transformCSVResults(results);
     mkdirp(path.dirname(filepath), function (err) {
         if (err) {
             console.error('\nError writing file: ' + filepath);
