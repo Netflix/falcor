4967ef16a9587c65a9d3d890d9f038ae431d3f7e
Paul Taylor <ptaylor@netflix.com>
2015-07-13 13:41:38 -0700

    Removes forgotten .only in tests.

--- test/falcor/invalidate/invalidate.change-handler.spec.js
+++ test/falcor/invalidate/invalidate.change-handler.spec.js
@@ -3,7 +3,7 @@ var Model = falcor.Model;
 var expect = require('chai').expect;
 
 describe("root onChange handler", function () {
-    it.only("is called when we invalidate a path", function () {
+    it("is called when we invalidate a path", function () {
 
         var changed = false;
         var model = new Model({
@@ -12,8 +12,6 @@ describe("root onChange handler", function () {
                 changed = true;
             }
         });
-
-        debugger;
 
         model.invalidate(["a", "b", "c"]);
 
