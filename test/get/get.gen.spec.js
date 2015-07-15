719308f51c0c4a7960ff6dec41702e50e2a85f4a
Paul Taylor <ptaylor@netflix.com>
2015-07-15 15:48:38 -0700

    getVersion should return -1 if no path exists, otherwise >= 0.

--- test/get/get.gen.spec.js
+++ test/get/get.gen.spec.js
@@ -7,17 +7,17 @@ describe('getVersionSync', function() {
         var model = new Model({cache: {hello: 'world'}});
         model._root.unsafeMode = true;
         var version = model.getVersion('hello');
-        expect(version > 0).to.be.ok;
+        expect(version >= 0).to.be.ok;
     });
     it('should get a version on the root model', function() {
         var model = new Model({cache: {hello: 'world'}, unsafeMode: true});
         var version = model.getVersion();
-        expect(version > 0).to.be.ok;
+        expect(version >= 0).to.be.ok;
     });
-    it('should get zero if no path exists.', function() {
+    it('should get -1 if no path exists.', function() {
         var model = new Model({cache: {hello: 'world'}});
         model._root.unsafeMode = true;
         var version = model.getVersion('world');
-        expect(version === 0).to.be.ok;
+        expect(version === -1).to.be.ok;
     });
 });
