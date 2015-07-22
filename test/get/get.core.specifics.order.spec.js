af4f39a02af8e8394741048ffc8ca37ec847f762
Michael Paulson <mpaulson@netflix.com>
2015-07-22 14:05:06 -0700

    clones now remain cache consistent with the source.

--- test/get/get.core.specifics.order.spec.js
+++ test/get/get.core.specifics.order.spec.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var noOp = function() {};
 
-describe('Order of operations.', function() {
+describe('Caching Issues', function() {
     it('should be able to use a model as a source.', function() {
         var source = new Model({
             cache: {
@@ -28,5 +28,29 @@ describe('Order of operations.', function() {
             return ;
         }
         expect(false, 'should never get here').to.be.ok;
+    });
+
+    it.only('should ensure that cache remains consistent amoung its clones.', function() {
+        var source = new Model({
+            cache: {
+                lolomo: {
+                    summary: 'this is a lolomo'
+                }
+            }
+        });
+        var clone = source.clone({});
+        source._root.unsafeMode = clone._root.unsafeMode = true;
+        var resSource = source._getPathSetsAsJSON(source, [['lolomo', 'summary']], [{}]);
+        var resClone = clone._getPathSetsAsJSON(clone, [['lolomo', 'summary']], [{}]);
+        expect(resClone).to.deep.equals(resSource);
+
+        source.setCache({
+            lolomo: {
+                name: 'Terminator 2'
+            }
+        });
+        resSource = source._getPathSetsAsJSON(source, [['lolomo', 'name']], [{}]);
+        resClone = clone._getPathSetsAsJSON(clone, [['lolomo', 'name']], [{}]);
+        expect(resClone).to.deep.equals(resSource);
     });
 });
