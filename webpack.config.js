ba4603dc6b51c011ab8a98a240beee1b6f6bf14d
Satyen Desai <satyend@netflix.com>
2015-07-24 18:38:18 -0700

    Added workaround for RSJS Webpack bug (specified in file)

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  resolve: {
+    alias: {
+      // Workaround https://github.com/Reactive-Extensions/RxJS/issues/832, until it's fixed
+      'rx$': require.resolve('rx/dist/rx')
+    }
+  }
+};
