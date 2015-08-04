c66bcc2eb0e50c809c841d8122002188b030a6bb
Falcor Build <falcor@netflix.com>
2015-08-04 13:24:28 -0700

    Updating build to push latest js to gh-pages

--- build/deploy-ghpages.sh
+++ build/deploy-ghpages.sh
@@ -5,9 +5,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   TEMP_DIR=$HOME/tmp
   FALCOR_DOCS_DIR=$TEMP_DIR/falcordocs
+  FALCOR_BUILD_DIR=$TEMP_DIR/falcorbuild
   GH_PAGES_DIR=$TEMP_DIR/gh-pages
 
   mkdir -p $TEMP_DIR
+
+  if [ -d "$FALCOR_BUILD_DIR" ]; then
+    rm -rf $FALCOR_BUILD_DIR
+  fi
 
   if [ -d "$FALCOR_DOCS_DIR" ]; then
     rm -rf $FALCOR_DOCS_DIR
@@ -19,8 +24,10 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   # Generate Docs
   npm run doc
+  npm run dist
 
   cp -R doc $FALCOR_DOCS_DIR
+  cp -R dist $FALCOR_BUILD_DIR
 
   # Change Working Directory to $HOME
   cd $HOME
@@ -34,10 +41,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cd $GH_PAGES_DIR
 
   rsync -r $FALCOR_DOCS_DIR/ doc/
+  rsync -r $FALCOR_BUILD_DIR/ build/
 
   git add .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push origin gh-pages
 
-  echo -e "Deployed docs to gh-pages\n"
+  echo -e "Deployed docs and build to gh-pages\n"
 fi
