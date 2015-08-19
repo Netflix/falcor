3cfc7d7a3888c97fa8e8450bb553dbddd9870adb
Jon Tewksbury <jontewks@gmail.com>
2015-08-18 21:45:45 -0700

    Fix eslint errors in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // Exports
@@ -6,20 +6,20 @@ module.exports = {
     coverage: coverage
 };
 
-function listen(port, launchWindow, cb) {
-    return app.listen(port, function () {
+function listen(serverPort, launchWindow, cb) {
+    return app.listen(serverPort, function() {
         if (cb) {
             cb();
         }
         if (launchWindow) {
-            require('child_process').exec('open http://localhost:' + port);
+            require("child_process").exec("open http://localhost:" + serverPort);
         }
     });
 }
 
-function coverage(port, launchWindow) {
-    app.use(express.static('coverage/lcov-report'));
-    return listen(port, launchWindow);
+function coverage(serverPort, launchWindow) {
+    app.use(express.static("coverage/lcov-report"));
+    return listen(serverPort, launchWindow);
 }
 
 // Run if main
@@ -27,17 +27,16 @@ if (require.main === module) {
     var port = process.argv[2] || 8080;
     var run = process.argv[3];
     switch (run) {
-        case 'examples':
-            app.use(express.static('.'));
-            app.get('/500', function(req, res) {
+        case "examples":
+            app.use(express.static("."));
+            app.get("/500", function(req, res) {
                 res.send(500);
             });
             listen(port, true);
             break;
-        case 'coverage':
+        case "coverage":
         default:
             coverage(port, true);
             break;
     }
 }
-
