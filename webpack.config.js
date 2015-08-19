b9492cf7f51313afe32fd924dd37304f175d84d9
Jon Tewksbury <jontewks@gmail.com>
2015-08-18 21:46:52 -0700

    Fix eslint in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   resolve: {
     alias: {
       // Workaround https://github.com/Reactive-Extensions/RxJS/issues/832, until it's fixed
-      'rx$': require.resolve('rx/dist/rx')
+      "rx$": require.resolve("rx/dist/rx")
     }
   }
 };
