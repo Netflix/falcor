831bc2e6e0519a37b9e951a8e8384f4705a82fee
Jon Tewksbury <jontewks@gmail.com>
2015-08-18 21:40:26 -0700

    Fix lint errors in browser.js

--- browser.js
+++ browser.js
@@ -1,11 +1,11 @@
-var falcor = require('./lib');
-var jsong = require('falcor-json-graph');
+var falcor = require("./lib");
+var jsong = require("falcor-json-graph");
 
 falcor.atom = jsong.atom;
 falcor.ref = jsong.ref;
 falcor.error = jsong.error;
 falcor.pathValue = jsong.pathValue;
 
-falcor.HttpDataSource = require('falcor-http-datasource');
+falcor.HttpDataSource = require("falcor-http-datasource");
 
 module.exports = falcor;
