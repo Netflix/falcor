3d4b650bfc5041bd2e2d074b74fc68344ba1bfc0
Michael Paulson <mpaulson@netflix.com>
2015-08-25 08:44:12 -0700

    Added the logic required to avoid array cloning when there is no complement.

--- test/internal/request/GetRequest.add.spec.js
+++ test/internal/request/GetRequest.add.spec.js
@@ -60,6 +60,55 @@ describe('#add', function() {
         var results = request.add([videos1234, videos553], [videos1234, videos553], zip);
     });
 
+    it('should send a request and dedupe another when dedupe is in second position.', function(done) {
+        var scheduler = new ImmediateScheduler();
+        var getSpy = sinon.spy();
+        var source = new LocalDataSource(Cache(), {
+            onGet: getSpy,
+            wait: 100
+        });
+        var model = new Model({source: source});
+        var request = new GetRequest(scheduler, {
+            removeRequest: function() { },
+            model: model
+        });
+
+        var zip = zipSpy(2, function() {
+            var onNext = sinon.spy();
+            model.
+                withoutDataSource().
+                get(videos1234, videos553).
+                doAction(onNext, noOp, function() {
+                    expect(getSpy.calledOnce).to.be.ok;
+                    expect(getSpy.getCall(0).args[1]).to.deep.equals([videos1234]);
+                    expect(onNext.calledOnce).to.be.ok;
+                    expect(onNext.getCall(0).args[0]).to.deep.equals({
+                        json: {
+                            videos: {
+                                1234: {
+                                    summary: {
+                                        title: 'House of Cards',
+                                        url: '/movies/1234'
+                                    }
+                                }
+                            }
+                        }
+                    });
+
+                    expect(results[0]).to.be.ok;
+                    expect(results[1], 'the requested complement should be 553').to.deep.equals([videos553]);
+                    expect(results[2], 'the optimized complement should be 553').to.deep.equals([videos553]);
+                }).
+                subscribe(noOp, done, done);
+        });
+
+        var disposable1 = request.batch([videos1234], [videos1234], zip);
+        expect(request.sent, 'request should be sent').to.be.ok;
+
+        var results = request.add([videos553, videos1234], [videos553, videos1234], zip);
+    });
+
+
     it('should send a request and dedupe another and dispose of original.', function(done) {
         var scheduler = new ImmediateScheduler();
         var getSpy = sinon.spy();
