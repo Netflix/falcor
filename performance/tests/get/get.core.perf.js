30fce9c5882f82d61af5570ef023d168ba8e3817
Michael Paulson <mpaulson@netflix.com>
2015-08-26 19:23:58 -0700

    working on the perf stuff.

--- performance/tests/get/get.core.perf.js
+++ performance/tests/get/get.core.perf.js
@@ -1,7 +1,7 @@
 var Model = require("./../../../lib").Model;
-var simple = [[0, 0, 'item', 'title']];
-var row = [[0, {from: 0, to: 9}, 'item', 'title']];
-var complex = [[{from: 0, to: 4}, {from: 0, to: 9}, 'item', 'title']];
+var simple = [['lolomo', 0, 0, 'item', 'title']];
+var row = [['lolomo', 0, {from: 0, to: 9}, 'item', 'title']];
+var complex = [['lolomo', {from: 0, to: 4}, {from: 0, to: 9}, 'item', 'title']];
 var noOp = function() {};
 var ImmediateScheduler = require("./../../../lib/schedulers/ImmediateScheduler");
 var CacheGenerator = require('./../../../test/CacheGenerator');
@@ -10,31 +10,25 @@ var model = new Model({
     cache: cache
 });
 
-model.deref(['lolomo'], [0, 0, 'item', 'title']).subscribe(function(m) {
-    model = m;
-});
-
-model.get(complex[0]).subscribe(function(data) { });
-
-var getAsPathMap = require('./../../../lib/get').getAsPathMap;
+var getWithPathsAsPathMap = require('./../../../lib/get').getWithPathsAsPathMap;
 
 module.exports = {
     'Test simple get': function() {
-        getAsPathMap(model, simple, [{}]);
+        getWithPathsAsPathMap(model, simple, [{}]);
     },
     'Test simple get again': function() {
-        getAsPathMap(model, simple, [{}]);
+        getWithPathsAsPathMap(model, simple, [{}]);
     },
     'Test row get': function() {
-        getAsPathMap(model, row, [{}]);
+        getWithPathsAsPathMap(model, row, [{}]);
     },
     'Test row get again': function() {
-        getAsPathMap(model, row, [{}]);
+        getWithPathsAsPathMap(model, row, [{}]);
     },
     'Test complex get': function() {
-        getAsPathMap(model, complex, [{}]);
+        getWithPathsAsPathMap(model, complex, [{}]);
     },
     'Test complex get again': function() {
-        getAsPathMap(model, complex, [{}]);
+        getWithPathsAsPathMap(model, complex, [{}]);
     }
 };
