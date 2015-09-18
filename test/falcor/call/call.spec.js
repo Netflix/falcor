e6b390157fcccfde2c8427158f9c6416b4934dbb
Michael Paulson <mpaulson@netflix.com>
2015-09-18 15:42:34 -0700

    CallResponse should be completed, but there are a few failing unit tests.

--- test/falcor/call/call.spec.js
+++ test/falcor/call/call.spec.js
@@ -1,20 +1,12 @@
 var falcor = require("./../../../lib/");
 var Model = falcor.Model;
-var $ref = Model.ref;
-var Cache = require("../../data/Cache");
-var ReducedCache = require("../../data/ReducedCache").ReducedCache;
 var LocalDataSource = require("../../data/LocalDataSource");
-var Expected = require("../../data/expected");
-var Rx = require("rx");
-var runGetTests = require("./../../getTestRunner").run;
-var testRunner = require("./../../testRunner");
-var getDataModel = testRunner.getModel;
+var strip = require("../../cleanData").stripDerefAndVersionKeys;
 var chai = require("chai");
+var sinon = require("sinon");
 var expect = chai.expect;
-var sinon = require('sinon');
 var noOp = function() {};
-var clean = require('./../../cleanData').clean;
-var cacheGenerator = require('./../../CacheGenerator');
+var ModelResponse = require('./../../../lib/response/ModelResponse');
 
 /**
  * @param newModel
@@ -25,132 +17,72 @@ function getModel(newModel, cache) {
 }
 
 describe("Call", function() {
-
     it("executes a remote call on a bound Model and sends the call and extra paths relative to the root", function(done) {
-        var model = getDataModel({
-            call: function(callPath, args, suffixes, extraPaths) {
-                expect(callPath).to.deep.equal(["lists", "add"]);
-                expect(extraPaths).to.deep.equal([[0, "summary"]]);
-                done();
+        var model = new Model({
+            source: {
+                call: function(callPath, args, suffixes, extraPaths) {
+                    expect(callPath).to.deep.equal(["lists", "add"]);
+                    expect(extraPaths).to.deep.equal([[0, "summary"]]);
+                    done();
+
+                    return {subscribe: noOp};
+                }
             }
-        }, Cache());
-        model
-            ._clone({ _path: ["lists"] })
-            .call(["add"], [], [], [[0, "summary"]])
-            .subscribe(noOp, noOp, noOp);
-    })
-
-    it("executes a local function with the call args", function(done) {
-
-        var model = getDataModel(new LocalDataSource(Cache()), ReducedCache());
-
-        model.withoutDataSource()._setValueSync(["lists", "my-list", "add"], function(videoID) {
-            return Rx.Observable.return({
-                path: [0],
-                value: $ref(["videos", videoID])
-            });
         });
 
         model.
-            call(["lists", "my-list", "add"], [1234], [["summary"]]).
-            subscribe(function(videos) {
-                testRunner.compare(videos[0], videos[1]);
-                done();
-            }, done);
+            _clone({ _path: ["lists"] }).
+            call(["add"], [], [], [[0, "summary"]]).
+            subscribe(noOp, noOp, noOp);
     });
 
-    it("executes a local function with call args and maps the result paths through a selector", function(done) {
-
-        var model = getDataModel(new LocalDataSource(Cache()), ReducedCache());
-        var onNext = sinon.spy();
-        model.withoutDataSource()._setValueSync(["lists", "my-list", "add"], function(videoID) {
-            return Rx.Observable.return({
-                path: [0],
-                value: $ref(["videos", videoID])
-            });
-        });
-
-        model.
-            call(["lists", "my-list", "add"], [1234], [["summary"]]).
-            concat(model.getValue(["lists", "my-list", 0, "summary"])).
-            last().
-            doAction(onNext).
-            doAction(noOp, noOp, function() {
-                expect(onNext.calledOnce).to.be.ok;
-                testRunner.compare({
-                    title: "House of Cards",
-                    url: "/movies/1234"
-                }, onNext.getCall(0).args[0]);
-            }).
-            subscribe(noOp, done, done);
-    });
-
-    it("executes a local function with call args on a bound model and emits invalidations relative to the optimized bound path", function(done) {
-
-        // callPath, args, suffix, paths
-
-        // The route info
-        var model = new Model({ cache: {
-            lolomo: { $type: "ref", value: ["lolomos", 123] },
-            lolomos: {
-                123: {
-                    add: function(listRef) {
-                        return this
-                            .setValue({ path: ["length"], value: 8})
-                            .flatMap(
-                                this.set({ path: [7], value: listRef }).
-                                    map(function(data) {
-                                        return {
-                                            path: [7],
-                                            value: data.json[7]
-                                        };
-                                    }))
-                            .concat(Rx.Observable["return"]({ path: [], invalidated: true }));
-                    }
+    it("ensures that invalidations are ran.", function(done) {
+        var model = new Model({
+            source: {
+                call: function(callPath, args, suffixes, extraPaths) {
+                    return new ModelResponse(function(observer) {
+                        observer.onNext({
+                            jsonGraph: {
+                                a: 'test'
+                            },
+                            paths: [['a']],
+                            invalidated: [['b']]
+                        });
+                        observer.onCompleted();
+                    });
                 }
             },
-            listsById: { 29: { name: "Horror" } }
-        }});
+            cache: {
+                a: 'foo',
+                b: 'test'
+            }
+        });
 
+        var onNext = sinon.spy();
+        var onNext2 = sinon.spy();
         model.
-            call("lolomo.add", [{ $type: "ref", value: ["listsById", 29] }], ["name"], ["length"]).
-            _toJSONG().
-            subscribe(function(envelope) {
-                var err;
-                try {
-                    testRunner.compare(getExpectedJSONG(), envelope);
-                } catch(e) {
-                    err = e;
-                } finally {
-                    done(err);
-                }
-            }, done);
-
-        function getExpectedJSONG() {
-            // The output json
-            return {
-                jsonGraph: {
-                    lolomo: { $size: "52", $type: "ref", value: ["lolomos", 123] },
-                    lolomos: {
-                        123: {
-                            7: { $size: "52", $type: "ref", value: ["listsById", 29] },
-                            "length": 8
-                        },
-                    },
-                    listsById: {
-                        29: {
-                            "name": "Horror",
-                        }
+            call(["test"], []).
+            doAction(onNext, noOp, function() {
+                expect(onNext.calledOnce).to.be.ok;
+                expect(strip(onNext.getCall(0).args[0])).to.deep.equals({
+                    json: {
+                        a: 'test'
                     }
-                },
-                paths: [
-                    ["lolomo", 7, "name"],
-                    ["lolomo", "length"]
-                ],
-                invalidated: [
-                    ["lolomos", 123]
-                ]
-            };
-        }
+                });
+            }).
+            flatMap(function() {
+                return model.
+                    withoutDataSource().
+                    get(['a'], ['b']);
+            }).
+            doAction(onNext2, noOp, function() {
+                expect(onNext2.calledOnce).to.be.ok;
+                expect(strip(onNext2.getCall(0).args[0])).to.deep.equals({
+                    json: {
+                        a: 'test'
+                    }
+                });
+            }).
+            subscribe(noOp, done, done);
     });
 });
