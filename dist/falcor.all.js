39f8dd6c5c96d1ebc2c055e225a6dbc86a5b5644
Falcor Build <falcorbuild@netflix.com>
2015-12-01 21:21:23 +0000

    Travis build 240 committed dist/

--- dist/falcor.all.js
+++ dist/falcor.all.js
@@ -8664,6 +8664,9 @@ module.exports = function matcher(rst) {
             throw err;
         }
 
+        // Reduce into groups multiple matched routes into route sets where
+        // each match matches the same route endpoint.  From here we can reduce
+        // the matched paths into the most optimal pathSet with collapse.
         var reducedMatched = matched.reduce(function(acc, matchedRoute) {
             if (!acc[matchedRoute.id]) {
                 acc[matchedRoute.id] = [];
@@ -8674,15 +8677,20 @@ module.exports = function matcher(rst) {
         }, {});
 
         var collapsedMatched = [];
+
+        // For every set of matched routes, collapse and reduce its matched set
+        // down to the minimal amount of collapsed sets.
         Object.
             keys(reducedMatched).
             forEach(function(k) {
                 var reducedMatch = reducedMatched[k];
 
-                // This one has no issues with collapsing, its ok to
-                // merge it back into the collapsedMatched array
+                // If the reduced match is of length one then there is no
+                // need to perform collapsing, as there is nothing to collapse
+                // over.
                 if (reducedMatch.length === 1) {
-                    return collapsedMatched.push(reducedMatch[0]);
+                    collapsedMatched.push(reducedMatch[0]);
+                    return;
                 }
 
                 // Since there are more than 1 routes, we need to see if
@@ -8694,8 +8702,12 @@ module.exports = function matcher(rst) {
                                     return x.requested;
                                 }));
 
+                // For every collapsed result we use the previously match result
+                // and update its requested and virtual path.  Then add that
+                // match to the collapsedMatched set.
                 collapsedResults.forEach(function(path, i) {
-                    var reducedVirtualPath = reducedMatch[i].virtual;
+                    var collapsedMatch = reducedMatch[i];
+                    var reducedVirtualPath = collapsedMatch.virtual;
                     path.forEach(function(atom, index) {
 
                         // If its not a routed atom then wholesale replace
@@ -8703,13 +8715,11 @@ module.exports = function matcher(rst) {
                             reducedVirtualPath[index] = atom;
                         }
                     });
+                    collapsedMatch.requested = path;
                     collapsedMatched.push(reducedMatch[i]);
                 });
             });
-        return {
-            matched: collapsedMatched,
-            missingPaths: missing
-        };
+        return collapsedMatched;
     };
 };
 
@@ -10056,10 +10066,21 @@ var isArray = Array.isArray;
  * json graph) or an async type (observable or a thenable).
  */
 module.exports = function outputToObservable(valueOrObservable) {
-    var value = valueOrObservable;
+    var value = valueOrObservable,
+        oldObservable;
 
     // place holder.  Observables have highest precedence.
-    if (value.subscribe) { }
+    if (value.subscribe) {
+        if (!(value instanceof Observable)) {
+            oldObservable = value;
+            value = Observable.create(function(observer) {
+                var sub = oldObservable.subscribe(observer);
+                return function() {
+                    sub.dispose();
+                }
+            });
+        }
+    }
 
     // promise
     else if (value.then) {
@@ -10316,12 +10337,11 @@ function _recurseMatchAndExecute(
                 return Observable.throw(e);
             }
 
-            if (!matchedResults.matched.length) {
+            if (!matchedResults.length) {
                 return Observable.empty();
             }
 
-            var matchedResult = matchedResults.matched;
-            return runByPrecedence(nextPaths, matchedResult, actionRunner).
+            return runByPrecedence(nextPaths, matchedResults, actionRunner).
 
                 // Generate from the combined results the next requestable paths
                 // and insert errors / values into the cache.
@@ -10384,7 +10404,6 @@ function _recurseMatchAndExecute(
                         pathsToExpand = collapse(pathsToExpand);
                     }
 
-                    missing = missing.concat(matchedResults.missingPaths);
                     return Observable.
                         from(pathsToExpand);
                 }).
