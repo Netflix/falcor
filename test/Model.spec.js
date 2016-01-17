719562e9cfad9b4f3db3c12a99995bea0a3d35d0
jhusain <jhusain@gmail.com>
2016-01-17 13:44:46 -0800

    Adding unit test that shows that unsubscribe is not invoked on the DataSource get operation when unsubscribing from the ModelResponse

--- test/Model.spec.js
+++ test/Model.spec.js
@@ -1,6 +1,51 @@
 var Rx = require("rx");
 var falcor = require("./../lib/");
 var Model = falcor.Model;
+
+function ResponseObservable(response) {
+    this.response = response;
+}
+
+ResponseObservable.prototype = Object.create(Rx.Observable.prototype);
+
+ResponseObservable.prototype._subscribe = function(observer) {
+    return this.response.subscribe(observer);
+};
+
+ResponseObservable.prototype._toJSONG = function() {
+    return new ResponseObservable(this.response._toJSONG.apply(this.response, arguments));
+};
+
+ResponseObservable.prototype.progressively = function() {
+    return new ResponseObservable(this.response.progressively.apply(this.response, arguments));
+};
+
+ResponseObservable.prototype.then = function() {
+    return this.response.then.apply(this.response, arguments);
+};
+
+
+var modelGet = Model.prototype.get;
+var modelSet = Model.prototype.set;
+var modelCall = Model.prototype.call;
+var modelPreload = Model.prototype.preload;
+
+Model.prototype.get = function() {
+    return new ResponseObservable(modelGet.apply(this, arguments));
+};
+
+Model.prototype.set = function() {
+    return new ResponseObservable(modelSet.apply(this, arguments));
+};
+
+Model.prototype.preload = function() {
+    return new ResponseObservable(modelPreload.apply(this, arguments));
+};
+
+Model.prototype.call = function() {
+    return new ResponseObservable(modelCall.apply(this, arguments));
+};
+
 var testRunner = require('./testRunner');
 var chai = require("chai");
 var expect = chai.expect;
@@ -8,6 +53,7 @@ var $ref = require('./../lib/types/ref');
 var $error = require('./../lib/types/error');
 var $atom = require('./../lib/types/atom');
 GLOBAL.toObservable = require('./toObs');
+var rxjs = require('rxjs');
 
 describe("Model", function() {
 
@@ -34,6 +80,78 @@ describe("Model", function() {
 
         out = Model.atom(1337);
         testRunner.compare({$type: $atom, value: 1337}, out);
+    });
+
+    it('unsubscribing should cancel DataSource request.', function(done) {
+        var onNextCalled = 0,
+            onErrorCalled = 0,
+            onCompletedCalled = 0,
+            unusubscribeCalled = 0,
+            dataSourceGetCalled = 0;
+
+        var model = new Model({
+            cache: {
+                list: {
+                    0: { name: "test" }
+                }
+            },
+            source: {
+                get: function() {
+                    return {
+                        subscribe: function(observerOrOnNext, onError, onCompleted) {
+                            dataSourceGetCalled++;
+                            var handle = setTimeout(function() {
+                                var response = {
+                                    jsonGraph: {
+                                        list: {
+                                            1: { name: "another test" }
+                                        }
+                                    },
+                                    paths: ["list", 1, "name"]
+                                };
+
+                                if (typeof observerOrOnNext === "function") {
+                                    observerOrOnNext(response);
+                                    onCompleted();
+                                }
+                                else {
+                                    observerOrOnNext.onNext(response);
+                                    observerOrOnNext.onCompleted();
+                                }
+                            });
+
+                            return {
+                                dispose: function() {
+                                    unusubscribeCalled++;
+                                    clearTimeout(handle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        var subscription = model.get("list[0,1].name").
+            subscribe(
+                function(value) {
+                    onNextCalled++;
+                },
+                function(error) {
+                    onErrorCalled++;
+                },
+                function() {
+                    onCompletedCalled++;
+                });
+
+        subscription.dispose();
+
+        if (dataSourceGetCalled === 1 && !onNextCalled && unusubscribeCalled === 1 && !onErrorCalled && !onCompletedCalled) {
+            done()
+        }
+        else {
+            done(new Error("DataSource unsubscribe not called."));
+        }
     });
 
     describe('JSON-Graph Specification', function() {
