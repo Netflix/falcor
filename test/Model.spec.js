111c458db054e21958dda4fa6ef2ec34a7fb4f2c
jhusain <jhusain@gmail.com>
2016-01-17 13:44:46 -0800

    Adding unit test that shows that unsubscribe is not invoked on the DataSource get operation when unsubscribing from the ModelResponse

--- test/Model.spec.js
+++ test/Model.spec.js
@@ -36,6 +36,78 @@ describe("Model", function() {
         testRunner.compare({$type: $atom, value: 1337}, out);
     });
 
+    it('unsubscribing should cancel DataSource request.', function(done) {
+        var onNextCalled = 0,
+            onErrorCalled = 0,
+            onCompletedCalled = 0,
+            unusubscribeCalled = 0,
+            dataSourceGetCalled = 0;
+
+        var model = new Model({
+            cache: {
+                list: {
+                    0: { name: "test" }
+                }
+            },
+            source: {
+                get: function() {
+                    return {
+                        subscribe: function(observerOrOnNext, onError, onCompleted) {
+                            dataSourceGetCalled++;
+                            var handle = setTimeout(function() {
+                                var response = {
+                                    jsonGraph: {
+                                        list: {
+                                            1: { name: "another test" }
+                                        }
+                                    },
+                                    paths: ["list", 1, "name"]
+                                };
+
+                                if (typeof observerOrOnNext === "function") {
+                                    observerOrOnNext(response);
+                                    onCompleted();
+                                }
+                                else {
+                                    observerOrOnNext.onNext(response);
+                                    observerOrOnNext.onCompleted();
+                                }
+                            });
+
+                            return {
+                                dispose: function() {
+                                    unusubscribeCalled++;
+                                    clearTimeout(handle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        var subscription = model.get("list[0,1].name").
+            subscribe(
+                function(value) {
+                    onNextCalled++;
+                },
+                function(error) {
+                    onErrorCalled++;
+                },
+                function() {
+                    onCompletedCalled++;
+                });
+
+        subscription.dispose();
+
+        if (dataSourceGetCalled === 1 && !onNextCalled && unusubscribeCalled === 1 && !onErrorCalled && !onCompletedCalled) {
+            done()
+        }
+        else {
+            done(new Error("DataSource unsubscribe not called."));
+        }
+    });
+
     describe('JSON-Graph Specification', function() {
         require('./get-core');
 
