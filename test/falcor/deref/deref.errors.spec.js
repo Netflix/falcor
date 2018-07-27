075d9b511895dd63d708e2cfe993a2ef9fc1d4a3
Anup Bishnoi <pixelsallover@gmail.com>
2018-07-27 14:26:14 -0700

    test(deref): added 2 tests for deref errors on invalidated paths
    1 test skipped because we need to make a decision about the behavior
    for set operation first.

--- test/falcor/deref/deref.errors.spec.js
+++ test/falcor/deref/deref.errors.spec.js
@@ -4,10 +4,10 @@ var InvalidDerefInputError = require('./../../../lib/errors/InvalidDerefInputErr
 var Model = falcor.Model;
 var sinon = require('sinon');
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 var cacheGenerator = require('./../../CacheGenerator');
 var noOp = function() {};
 var isAssertionError = require('./../../isAssertionError');
-var clean = require("../../cleanData").clean;
 
 describe('Error cases', function() {
     it('should error on a shorted deref path.', function(done) {
@@ -43,7 +43,7 @@ describe('Error cases', function() {
                             if (isAssertionError(err)) {
                                 return done(err);
                             }
-                            done();
+                            return done();
                         },
                         done.bind(null, new Error('onCompleted shouldnt be called')));
             });
@@ -56,6 +56,33 @@ describe('Error cases', function() {
             expect(e.name).to.equals(InvalidDerefInputError.name);
             return done();
         }
-        done(new Error('should of thrown an error.'));
+        return done(new Error('should of thrown an error.'));
+    });
+
+    it('should throw InvalidModelError on an invalidated deref path.', function() {
+        var model = new Model({cache: {titlesById: {32: {name: "House of Cards"}}}});
+        return model.get(["titlesById", 32, "name"]).
+            then(function(response) {
+                var titleModel = model.deref(response.json.titlesById[32]);
+                model.invalidate(["titlesById"]);
+                return titleModel.get(["name"]).
+                    then(assert.fail).
+                    catch(function(err) {
+                        expect(err.message).to.equals(InvalidModelError.message);
+                    });
+            });
+    });
+
+    it.skip('should not error on an invalidated deref path set.', function() {
+        var model = new Model({cache: {titlesById: {32: {name: "House of Cards"}}}});
+        return model.get(["titlesById", 32, "name"]).
+            then(function(response) {
+                var titleModel = model.deref(response.json.titlesById[32]);
+                model.invalidate(["titlesById", 32]);
+                return titleModel.set({json: {name: "Something Else"}}).
+                    then(function(derefedResponse) {
+                        expect(response.json.name).to.be("Something Else");
+                    });
+            });
     });
 });
