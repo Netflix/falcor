9c4dbc841d8546623e4d83f45231f4c4771b8f4e
Jeroen Cranendonk <jcranendonk@netflix.com>
2018-11-14 19:41:37 -0800

    ES6-ified and fixed some tests

--- test/integration/dedupe.spec.js
+++ test/integration/dedupe.spec.js
@@ -1,211 +1,218 @@
-var falcor = require('../../browser');
-var LocalDataSource = require('../data/LocalDataSource');
-var after = require('after');
-var expect = require('chai').expect;
-var sinon = require('sinon');
-var strip = require('../cleanData').stripDerefAndVersionKeys;
-var cacheGenerator = require('../CacheGenerator');
+const falcor = require("../../browser");
+const LocalDataSource = require("../data/LocalDataSource");
+const after = require("lodash/after");
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const strip = require("../cleanData").stripDerefAndVersionKeys;
+const cacheGenerator = require("../CacheGenerator");
 
-var $ref = falcor.Model.ref;
-var noOp = function() {};
+const noOp = () => {};
 
-describe('Request deduping', function() {
-    it('should dedupe new requested paths with previous in-flight requests', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: 'thing: 0',
-                    1: 'thing: 1',
-                    2: 'thing: 2'
-                }
-            }, {
-                wait: 0, // wait: 0 means setTimeout 0
-                onGet: onGet
-            })
-        });
-        var partDone = after(2, done);
-
-        model.get(['things', { from: 0, to: 1 }]).subscribe(function(response) {
-            expect(onGet.calledOnce).to.equal(true);
-            expect(onGet.getCall(0).args[1]).to.deep.equal([[ 'things', { from: 0, to: 1 } ]]);
-            expect(strip(response.json)).to.deep.equal({
-                things: {
-                    0: 'thing: 0',
-                    1: 'thing: 1'
-                }
-            });
-        }, done, partDone);
-        model.get(['things', { from: 1, to: 2 }]).subscribe(function(response) {
-            expect(onGet.calledTwice).to.equal(true);
-            expect(onGet.getCall(1).args[1]).to.deep.equal([[ 'things', '2' ]]);
-            expect(strip(response.json)).to.deep.equal({
-                things: {
-                    1: 'thing: 1',
-                    2: 'thing: 2'
-                }
-            });
-        }, done, partDone);
-    });
-
-    it('should dedupe from both ends of overlapping ranges', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: 'thing: 0',
-                    1: 'thing: 1',
-                    2: 'thing: 2',
-                    3: 'thing: 3',
-                    4: 'thing: 4',
-                }
-            }, {
-                wait: 0,
-                onGet: onGet
-            })
-        });
-        var partDone = after(2, done);
-
-        model.get(['things', { from: 1, to: 2 }]).subscribe(function(response) {
-            expect(onGet.getCall(0).args[1]).to.deep.equal([[ 'things', { from: 1, to: 2 } ]]);
-        }, done, partDone);
-        model.get(['things', { from: 0, to: 4 }]).subscribe(function(response) {
-            expect(onGet.getCall(1).args[1]).to.deep.equal([[ 'things', [0, 3, 4] ]]);
-        }, done, partDone);
-    });
-
-    it('should leave ranges unrolled if possible', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: 'thing: 0',
-                    1: 'thing: 1',
-                    2: 'thing: 2',
-                    3: 'thing: 3',
-                    4: 'thing: 4',
-                }
-            }, {
-                wait: 0,
-                onGet: onGet
-            })
-        });
-        var partDone = after(2, done);
-
-        model.get(['things', { from: 0, to: 2 }]).subscribe(noOp, done, partDone);
-        model.get(['things', { from: 0, to: 4 }]).subscribe(function(response) {
-            expect(onGet.getCall(1).args[1]).to.deep.equal([[ 'things', { from: 3, to: 4 } ]]);
-        }, done, partDone);
-    });
-
-    it('should work for properties after ranges', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: { name: 'thing: 0' },
-                    1: { name: 'thing: 1' },
-                    2: { name: 'thing: 2' },
-                }
-            }, {
-                wait: 0,
-                onGet: onGet
-            })
-        });
-        var partDone = after(2, done);
-
-        model.get(['things', { from: 0, to: 1 }, 'name']).subscribe(noOp, done, partDone);
-        model.get(['things', { from: 1, to: 2 }, 'name']).subscribe(function() {
-            expect(onGet.getCall(1).args[1]).to.deep.equal([[ 'things', 2, 'name' ]]);
-        }, done, partDone);
-    });
-
-    it('should work for multiple ranges in path sets', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: { name: 'thing: 0', tags: { 0: 't0 tag: 0', 1: 't0 tag: 1', 2: 't0 tag: 2' } },
-                    1: { name: 'thing: 1', tags: { 0: 't1 tag: 0', 1: 't1 tag: 1', 2: 't1 tag: 2' } },
-                    2: { name: 'thing: 2', tags: { 0: 't2 tag: 0', 1: 't2 tag: 1', 2: 't2 tag: 2' } },
-                }
-            }, {
-                wait: 0,
-                onGet: onGet
-            })
-        });
-        var partDone = after(2, done);
-
-        model.get(['things', { from: 0, to: 1 }, 'tags', { from: 1, to: 2 }]).subscribe(noOp, done, partDone);
-        model.get(['things', { from: 0, to: 2 }, 'tags', { from: 0, to: 2 }]).subscribe(function() {
-            expect(onGet.getCall(1).args[1]).to.deep.equal([
-                ['things', { from: 0, to: 1 }, 'tags', '0'],
-                ['things', 2, 'tags', { from: 0, to: 2 }]
-            ]);
-        }, done, partDone);
-    });
-
-    it('should work when different optimized and requested path lengths', function(done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({
-            source: new LocalDataSource({
-                things: {
-                    0: {
-                        tags: {
-                            0: 't0 tag: 0',
-                            1: 't0 tag: 1'
-                        }
-                    },
-                },
-                oneoff: {
-                    tags: {
-                        0: 't2 tag: 0',
-                        1: 't2 tag: 1'
+describe("Request deduping", () => {
+    it("should dedupe new requested paths with previous in-flight requests", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: "thing: 0",
+                        1: "thing: 1",
+                        2: "thing: 2"
                     }
                 },
-                thang: {
-                    that: {
-                        really: {
-                            is: {
-                                a: {
-                                    thing: {
-                                        of: {
-                                            course: {
-                                                tags: {
-                                                    0: 'thang tag: 0',
-                                                    1: 'thang tag: 1' }}}}}}}}}
-            }, {
-                wait: 0,
-                onGet: onGet
-            }),
+                { onGet }
+            )
+        });
+
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([["things", "2"]]);
+
+            done();
+        });
+
+        model
+            .get(["things", { from: 0, to: 1 }])
+            .subscribe(
+                response => expect(strip(response.json)).to.deep.equal({ things: { 0: "thing: 0", 1: "thing: 1" } }),
+                done,
+                partDone
+            );
+
+        model
+            .get(["things", { from: 1, to: 2 }])
+            .subscribe(
+                response => expect(strip(response.json)).to.deep.equal({ things: { 1: "thing: 1", 2: "thing: 2" } }),
+                done,
+                partDone
+            );
+    });
+
+    it("should dedupe from both ends of overlapping ranges", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: "thing: 0",
+                        1: "thing: 1",
+                        2: "thing: 2",
+                        3: "thing: 3"
+                    }
+                },
+                { onGet }
+            )
+        });
+
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([["things", [0, 3]]]);
+
+            done();
+        });
+
+        model.get(["things", { from: 1, to: 2 }]).subscribe(noOp, done, partDone);
+        model.get(["things", { from: 0, to: 3 }]).subscribe(noOp, done, partDone);
+    });
+
+    it("should leave ranges unrolled if possible", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: "thing: 0",
+                        1: "thing: 1",
+                        2: "thing: 2",
+                        3: "thing: 3"
+                    }
+                },
+                { onGet }
+            )
+        });
+
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([["things", { from: 2, to: 3 }]]);
+            done();
+        });
+
+        model.get(["things", { from: 0, to: 1 }]).subscribe(noOp, done, partDone);
+        model.get(["things", { from: 0, to: 3 }]).subscribe(noOp, done, partDone);
+    });
+
+    it("should work for properties after ranges", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: { name: "thing: 0" },
+                        1: { name: "thing: 1" },
+                        2: { name: "thing: 2" },
+                        3: { name: "thing: 3" }
+                    }
+                },
+                { onGet }
+            )
+        });
+
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([["things", { from: 2, to: 3 }, "name"]]);
+            done();
+        });
+
+        model.get(["things", { from: 0, to: 1 }, "name"]).subscribe(noOp, done, partDone);
+        model.get(["things", { from: 1, to: 3 }, "name"]).subscribe(noOp, done, partDone);
+    });
+
+    it("should work for multiple ranges in path sets", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: { name: "thing: 0", tags: { 0: "t0 tag: 0", 1: "t0 tag: 1", 2: "t0 tag: 2" } },
+                        1: { name: "thing: 1", tags: { 0: "t1 tag: 0", 1: "t1 tag: 1", 2: "t1 tag: 2" } },
+                        2: { name: "thing: 2", tags: { 0: "t2 tag: 0", 1: "t2 tag: 1", 2: "t2 tag: 2" } }
+                    }
+                },
+                { onGet }
+            )
+        });
+
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([
+                ["things", { from: 0, to: 1 }, "tags", "0"],
+                ["things", 2, "tags", { from: 0, to: 2 }]
+            ]);
+
+            done();
+        });
+
+        model.get(["things", { from: 0, to: 1 }, "tags", { from: 1, to: 2 }]).subscribe(noOp, done, partDone);
+        model.get(["things", { from: 0, to: 2 }, "tags", { from: 0, to: 2 }]).subscribe(noOp, done, partDone);
+    });
+
+    it("should work when different optimized and requested path lengths", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({
+            source: new LocalDataSource(
+                {
+                    things: {
+                        0: {
+                            tags: {
+                                0: "t0 tag: 0",
+                                1: "t0 tag: 1"
+                            }
+                        }
+                    },
+                    oneoff: {
+                        tags: {
+                            0: "t2 tag: 0",
+                            1: "t2 tag: 1"
+                        }
+                    },
+                    thang: {
+                        that: {
+                            really: {
+                                is: {
+                                    a: { thing: { of: { course: { tags: { 0: "thang tag: 0", 1: "thang tag: 1" } } } } }
+                                }
+                            }
+                        }
+                    }
+                },
+                { onGet }
+            ),
             cache: {
                 things: {
-                    1: $ref('thang.that.really.is.a.thing.of.course'),
-                    2: $ref('oneoff')
+                    1: falcor.Model.ref("thang.that.really.is.a.thing.of.course"),
+                    2: falcor.Model.ref("oneoff")
                 }
             }
         });
-        var partDone = after(2, done);
 
-        model.get(['things', 0, 'tags', 0]).subscribe(noOp, done, partDone);
+        const partDone = after(2, () => {
+            expect(onGet.getCall(1).args[1]).to.deep.equal([
+                ["oneoff", "tags", { from: 0, to: 1 }],
+                ["things", 0, "tags", "1"],
+                ["thang", "that", "really", "is", "a", "thing", "of", "course", "tags", { from: 0, to: 1 }]
+            ]);
+
+            done();
+        });
+
+        model.get(["things", 0, "tags", 0]).subscribe(noOp, done, partDone);
         // path length differences:
         // things[0].tags[0,1] -> requested: 4, optimized: 4
         // things[1].tags[0,1] -> requested: 4, optimized: 10
         // things[2].tags[0,1] -> requested: 4, optimized: 3
-        model.get(['things', { from: 0, to: 2 }, 'tags', { from: 0, to: 1 }]).subscribe(function(response) {
-            expect(onGet.getCall(1).args[1]).to.deep.equal([
-                ['oneoff', 'tags', { from: 0, to: 1 }],
-                ['things', 0, 'tags', '1'],
-                ['thang', 'that', 'really', 'is', 'a', 'thing', 'of', 'course', 'tags', { from: 0, to: 1 }]
-            ]);
-        }, done, partDone);
+        model.get(["things", { from: 0, to: 2 }, "tags", { from: 0, to: 1 }]).subscribe(noOp, done, partDone);
     });
 
-    it("deduplicates gets with overlapping ranges", function (done) {
-        var onGet = sinon.spy();
-        var model = new falcor.Model({ source: new LocalDataSource(cacheGenerator(0, 3), { wait: 0, onGet: onGet }) });
+    it("deduplicates gets with overlapping ranges", done => {
+        const onGet = sinon.spy();
+        const model = new falcor.Model({ source: new LocalDataSource(cacheGenerator(0, 3), { onGet }) });
 
-        var partDone = after(3, function() {
+        const partDone = after(3, () => {
             expect(onGet.getCall(0).args[1]).to.deep.equal([["videos", 0, "title"]]);
             expect(onGet.getCall(1).args[1]).to.deep.equal([["videos", 1, "title"]]);
             expect(onGet.getCall(2).args[1]).to.deep.equal([["videos", 2, "title"]]);
@@ -218,4 +225,3 @@ describe('Request deduping', function() {
         model.get(["videos", [0, 1, 2], "title"]).subscribe(noOp, done, partDone);
     });
 });
-
--- test/internal/request/complement.spec.js
+++ test/internal/request/complement.spec.js
@@ -1,42 +1,50 @@
-var expect = require("chai").expect;
-var complement = require("../../../lib/request/complement");
-var findPartialIntersections = require("../../../lib/request/complement").findPartialIntersections;
+const expect = require("chai").expect;
+const complement = require("../../../lib/request/complement");
+const findPartialIntersections = require("../../../lib/request/complement").__test.findPartialIntersections;
 
-describe("complement", function() {
-    it("returns null if no paths are provided", function() {
-        expect(complement([], [], {})).to.be.null;
+describe("complement", () => {
+    it("handles empty path sets", () => {
+        expect(complement([], [], {})).to.deep.equal({
+            intersection: [],
+            optimizedComplement: [],
+            requestedComplement: []
+        });
     });
 
-    it("returns null if no deduping was possible", function() {
-        var paths = [["videos", 0, "title"]];
-        expect(complement(paths, paths, {})).to.be.null;
+    it("returns all paths if no deduping possible", () => {
+        const paths = [["videos", 0, "title"]];
+        expect(complement(paths, paths, {})).to.deep.equal({
+            intersection: [],
+            optimizedComplement: paths,
+            requestedComplement: paths
+        });
     });
 
-    it("returns the complement and intersection consisting of paths that can be partially deduped", function() {
-        var partialMatchingPath = ["videos", [0, 1], "title"];
-        var paths = [partialMatchingPath];
-        var pathTree = { "3": { videos: { "0": { title: null } } } };
+    it("returns the complement and intersection consisting of paths that can be partially deduped", () => {
+        const partialMatchingPath = ["videos", [0, 1], "title"];
+        const paths = [partialMatchingPath];
+        const pathTree = { 3: { videos: { 0: { title: null } } } };
 
-        expect(complement(paths, paths, pathTree)).to.deep.equal([
-            [["videos", 0, "title"]],
-            [["videos", 1, "title"]],
-            [["videos", 1, "title"]]
-        ]);
+        expect(complement(paths, paths, pathTree)).to.deep.equal({
+            intersection: [["videos", 0, "title"]],
+            optimizedComplement: [["videos", 1, "title"]],
+            requestedComplement: [["videos", 1, "title"]]
+        });
     });
 });
 
-describe("findPartialIntersections", function() {
-    var matchingPath;
-    var matchingPathTree;
+describe("findPartialIntersections", () => {
+    let matchingPath;
+    let matchingPathTree;
 
-    beforeEach(function() {
+    beforeEach(() => {
         matchingPath = ["lolomo", 123, "summary"];
-        matchingPathTree = { lolomo: { "123": { summary: null } } };
+        matchingPathTree = { lolomo: { 123: { summary: null } } };
     });
 
-    it("returns paths if no deduping was possible", function() {
-        var requestedPath = ["videos", 0, "title"];
-        var optimizedPath = ["videosById", 1232, "title"];
+    it("returns paths if no deduping was possible", () => {
+        const requestedPath = ["videos", 0, "title"];
+        const optimizedPath = ["videosById", 1232, "title"];
 
         expect(findPartialIntersections(requestedPath, optimizedPath, {})).to.deep.equal([
             [],
@@ -45,7 +53,7 @@ describe("findPartialIntersections", function() {
         ]);
     });
 
-    it("returns the intersection consisting of paths that can be fully deduped", function() {
+    it("returns the intersection consisting of paths that can be fully deduped", () => {
         expect(findPartialIntersections(matchingPath, matchingPath, matchingPathTree)).to.deep.equal([
             [matchingPath],
             [],
@@ -53,18 +61,14 @@ describe("findPartialIntersections", function() {
         ]);
     });
 
-    describe("with optimized paths shorted than requested paths", function() {
-        it("returns the complement and intersection consisting of paths than can be partially deduped", function() {
-            var partialMatchingRequestedPath = ["lolomo", 123, 0, 0, ["title", "boxart"]];
-            var partialMatchingOptimizedPath = ["videosById", 456, ["title", "boxart"]];
-            var pathTree = { videosById: { "456": { title: null } } };
+    describe("with optimized paths shorter than requested paths", () => {
+        it("returns the complement and intersection consisting of paths than can be partially deduped", () => {
+            const partialMatchingRequestedPath = ["lolomo", 123, 0, 0, ["title", "boxart"]];
+            const partialMatchingOptimizedPath = ["videosById", 456, ["title", "boxart"]];
+            const pathTree = { videosById: { 456: { title: null } } };
 
             expect(
-                findPartialIntersections(
-                    partialMatchingRequestedPath,
-                    partialMatchingOptimizedPath,
-                    pathTree
-                )
+                findPartialIntersections(partialMatchingRequestedPath, partialMatchingOptimizedPath, pathTree)
             ).to.deep.equal([
                 [["lolomo", 123, 0, 0, "title"]],
                 [["videosById", 456, "boxart"]],
@@ -73,18 +77,14 @@ describe("findPartialIntersections", function() {
         });
     });
 
-    describe("with optimized paths longer than requested paths", function() {
-        it("returns the complement and intersection consisting of paths than can be partially deduped", function() {
-            var partialMatchingRequestedPath = ["videos", 123, ["title", "boxart"]];
-            var partialMatchingOptimizedPath = ["some", "weird", "long", "ref", 456, ["title", "boxart"]];
-            var pathTree = { some: { weird: { long: { ref: { "456": { title: null } } } } } };
+    describe("with optimized paths longer than requested paths", () => {
+        it("returns the complement and intersection consisting of paths than can be partially deduped", () => {
+            const partialMatchingRequestedPath = ["videos", 123, ["title", "boxart"]];
+            const partialMatchingOptimizedPath = ["some", "weird", "long", "ref", 456, ["title", "boxart"]];
+            const pathTree = { some: { weird: { long: { ref: { 456: { title: null } } } } } };
 
             expect(
-                findPartialIntersections(
-                    partialMatchingRequestedPath,
-                    partialMatchingOptimizedPath,
-                    pathTree
-                )
+                findPartialIntersections(partialMatchingRequestedPath, partialMatchingOptimizedPath, pathTree)
             ).to.deep.equal([
                 [["videos", 123, "title"]],
                 [["some", "weird", "long", "ref", 456, "boxart"]],
@@ -92,18 +92,12 @@ describe("findPartialIntersections", function() {
             ]);
         });
 
-        it("halts descent into the subtree", function() {
-            var requestedPath = ["videos", 123, "title"];
-            var optimizedPath = ["some", "weird", "long", "ref", 456, "title"];
-            var pathTree = { some: { differentPath: null } };
+        it("halts descent into the subtree", () => {
+            const requestedPath = ["videos", 123, "title"];
+            const optimizedPath = ["some", "weird", "long", "ref", 456, "title"];
+            const pathTree = { some: { differentPath: null } };
 
-            expect(
-                findPartialIntersections(
-                    requestedPath,
-                    optimizedPath,
-                    pathTree
-                )
-            ).to.deep.equal([
+            expect(findPartialIntersections(requestedPath, optimizedPath, pathTree)).to.deep.equal([
                 [],
                 [optimizedPath],
                 [requestedPath]
